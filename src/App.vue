fc4612fd9a490cca47c10c83cac971cd29297a93
Devon Scoles <Scolesdevon@gmail.com>
2024-08-14 16:42:36 -0400

    Job listing website created. View All jobs button needs fixing

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
+<script setup>
+import NavBar from '@/components/NavBar.vue';
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import JobListings from './components/JobListings.vue';
+</script>
+
 <template>
-    <h1 class="text-2xl">Vue Jobs</h1>
+    <NavBar />
+    <Hero />
+    <HomeCards />
+    <JobListings :limit="3" :showButton="true" />
 </template>
