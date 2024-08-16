88a5c51b825852e98735290813ba53f14825fb7b
Devon Scoles <Scolesdevon@gmail.com>
2024-08-16 01:44:57 -0400

    All views added to the router along with button functionality implemented

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,7 +1,9 @@
 <script setup>
-import jobData from '@/jobs.json';
-import { ref, defineProps } from 'vue';
+import { RouterLink } from 'vue-router';
+import { reactive, defineProps, onMounted } from 'vue';
 import JobListing from './JobListing.vue';
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
+import axios from 'axios';
 
 defineProps({
     limit: Number,
@@ -11,7 +13,22 @@ defineProps({
     }
 });
 
-const jobs = ref(jobData);
+const state = reactive({
+    jobs: [],
+    isLoading: true,
+});
+
+onMounted(async () => {
+    try {
+        const response = await axios.get('/api/jobs');
+        state.jobs = response.data; 
+        //change port above depending on what port you selected on line 10 of package.json
+    } catch(error){
+        console.error('Error fetching jobs', error);
+    } finally {
+        state.isLoading = false;
+    }
+});
 </script>
 
 <template>
@@ -20,8 +37,14 @@ const jobs = ref(jobData);
             <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
                 Browse Jobs
             </h2>
+            <!-- Show loading spinner/dots while loading is true -->
+             <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+                <PulseLoader />
+             </div>
+
+             <!-- Show job listing when done loading -->
             <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <JobListing v-for="job in jobs.slice(0, limit || jobs.value.length)" 
+                <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" 
                 :key="job.id" 
                 :job="job" 
                 />
@@ -30,11 +53,10 @@ const jobs = ref(jobData);
     </section>
 
     <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-      <a
-        href="/jobs"
+      <RouterLink
+        to="/jobs"
         class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
+        >View All Jobs</RouterLink>
     </section>
 
 </template>
